29660ce54fd523a8bd288a88f57db7b94e8d7c0d
elvisBM <elvisbmartins@gmail.com>
2018-11-06 19:26:59 -0200

    aula 1

---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import{ Component } from '@angular/core';
+
+@Component({
+    selector : 'foto',
+    template : '<img src="/assets/images/cerveja.png" alt="cerveja e corrida" >'
+    //stylesUrls : ['./foto.component.css']
+})
+export class FotoComponent{
+
+}
