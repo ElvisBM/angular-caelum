1351faa595464a72da06613228041ff3cdeac136
elvisBM <elvisbmartins@gmail.com>
2018-11-07 20:50:13 -0200

    component listagem gerado atraves do cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CardModule } from './card/card.module';
+import { ListagemComponent } from './listagem/listagem.component';
  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/listagem/listagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'caelumpic-listagem',
+  templateUrl: './listagem.component.html',
+  styles: []
+})
+export class ListagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
