9346e232e6234a648b433c3eca961ef5c60b2779
Elvis B. Martins <elvisbmartins@gmail.com>
2018-11-08 14:54:55 -0200

    criado Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ListagemComponent } from './listagem/listagem.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ReordenaComponent } from './reordena/reordena.component';
 
 const rotasApp:Routes =[
     {path:'', component: ListagemComponent},
     {path:'cadastro', component: CadastroComponent },
+    {path:'reordena', component: ReordenaComponent},
     {path: '**', redirectTo: ''}
 ]
 
