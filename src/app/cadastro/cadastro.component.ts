5130144736ed13df2068a5abd575f102e37ffcb9
elvisBM <elvisbmartins@gmail.com>
2018-11-08 19:34:01 -0200

    add post no cadastro para salvar os dados no servidor

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Foto } from '../foto/foto';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'caelumpic-cadastro',
@@ -10,11 +11,14 @@ export class CadastroComponent implements OnInit {
 
   foto = new Foto();
 
-  constructor() { }
+  constructor(private conexaoApi: HttpClient) { }
 
   ngOnInit() {
   }
   salvar(){
     console.log(this.foto);
+    this.conexaoApi
+    .post('http://localhost:3000/v1/fotos', this.foto)
+    .subscribe()
   }
 }
