fe078531ef54c1e60bce64cdd213fb113d37e069
elvisBM <elvisbmartins@gmail.com>
2018-11-08 19:34:36 -0200

    reordena add material no package e mudanca de porta do server

--- src/app/reordena/reordena.component.ts
+++ src/app/reordena/reordena.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { moveItemInArray } from '@angular/cdk/drag-drop';
+import { moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'caelumpic-reordena',
@@ -10,7 +10,7 @@ import { moveItemInArray } from '@angular/cdk/drag-drop';
 export class ReordenaComponent implements OnInit {
   listaFotos
   constructor( conexaoApi: HttpClient ){
-    conexaoApi.get('http://localhost:52874/v1/fotos')
+    conexaoApi.get('http://localhost:3000/v1/fotos')
     .subscribe(
         // Arrow Function
         fotosApi => {
