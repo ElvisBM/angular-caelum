3c1e12f744b6a5aada7ccfa51e190753261ae7a4
Elvis B. Martins <elvisbmartins@gmail.com>
2018-11-09 17:37:28 -0200

    save reordena

--- src/app/reordena/reordena.component.ts
+++ src/app/reordena/reordena.component.ts
@@ -4,26 +4,40 @@ import { FotoService } from '../servicos/foto.service';
 import { ListagemComponent } from '../listagem/listagem.component';
 
 @Component({
-  selector: 'caelumpic-reordena',
-  templateUrl: './reordena.component.html',
-  styles: []
+	selector: 'caelumpic-reordena',
+	templateUrl: './reordena.component.html',
+	styles: []
 })
 export class ReordenaComponent implements OnInit {
-  //lista:ListagemComponent = new ListagemComponent;
-  listaFotos
-  constructor( private fotoService:FotoService ){
-    this.fotoService.listar()
-    .subscribe(
-        // Arrow Function
-        fotosApi => {
-            this.listaFotos = fotosApi
-        }    
-    )
-  }
-  ngOnInit() {
-  }
-  onDrop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.listaFotos, event.previousIndex, event.currentIndex);
-  }
+	//lista:ListagemComponent = new ListagemComponent;
+	listaFotos
+	constructor( private fotoService:FotoService ){
+		this.fotoService.listaReordenar()
+		.subscribe(
+			// Arrow Function
+			fotosApi => {
+					this.listaFotos = fotosApi
+			}    
+		)
+	}
+	ngOnInit() {
+	}
+	onDrop(event: CdkDragDrop<string[]>) {
+		moveItemInArray(this.listaFotos, event.previousIndex, event.currentIndex);
+	}
+	salvarOrdem(){
+		console.log(this.listaFotos);
+		this.fotoService
+				.reordenar(this.listaFotos)
+				.subscribe(
+					(resposta)=>{
+						console.log(resposta);
+					},
+					erro => console.log(erro),
+					()=> console.log('completou')   
+				)
+			}
+		}
+	}
 
 }
