8771abc90798f2089a945c487d7b961971773038
elvisBM <elvisbmartins@gmail.com>
2018-11-09 19:52:25 -0200

    validandose o fotoId existe/if

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -4,42 +4,48 @@ import { FotoService } from '../servicos/foto.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'caelumpic-cadastro',
-  templateUrl: './cadastro.component.html',
-  styles: []
+	selector: 'caelumpic-cadastro',
+	templateUrl: './cadastro.component.html',
+	styles: []
 })
 export class CadastroComponent implements OnInit {
 
-  foto = new Foto();
+	foto = new Foto();
 
-  constructor(private fotoService: FotoService,
-              private roteador: Router,Reorder
-              private rotaAtiva: ActivatedRoute){ }
+	constructor(private fotoService: FotoService,
+							private roteador: Router,
+							private rotaAtiva: ActivatedRoute){ }
 
-  ngOnInit() {
-    /*
-    this.rotaAtiva.params.subscribe(
-      parametros => {
-        parametros.fotoId
-      }
-    )*/
+	ngOnInit() {
+		/*
+		this.rotaAtiva.params.subscribe(
+			parametros => {
+				parametros.fotoId
+			}
+		)*/
 
-    let fotoId = this.rotaAtiva.snapshot.params.fotoId;
+		let fotoId = this.rotaAtiva.snapshot.params.fotoId;
+		if(fotoId){
+			this.fotoService.buscar(fotoId).subscribe(
+				resposta => {
+					console.log(resposta);
+					this.foto = resposta;
+				}
+			);
+		}
+	};
 
-    this.fotoService.buscar(fotoId)
-
-  }
-  salvar(){
-    console.log(this.foto);
-    this.fotoService
-    .cadastrar(this.foto)
-    .subscribe(
-      (resposta)=>{
-        console.log(resposta);
-        this.roteador.navigate([]);
-      },
-      erro => console.log(erro),
-      ()=> console.log('completou')   
-    )
-  }
+	salvar(){
+		console.log(this.foto);
+		this.fotoService
+		.cadastrar(this.foto)
+		.subscribe(
+			(resposta)=>{
+				console.log(resposta);
+				this.roteador.navigate([]);
+			},
+			erro => console.log(erro),
+			()=> console.log('completou')   
+		)
+	}
 }
