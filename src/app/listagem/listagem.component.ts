d785faf78a89f292d65ffc6b68efdc860d3601af
elvisBM <elvisbmartins@gmail.com>
2018-11-08 22:43:12 -0200

    filter par retirar a foto apagada da listaFoto

--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -3,35 +3,42 @@ import { FotoService } from '../servicos/foto.service';
 import { Foto } from '../foto/foto';
 
 @Component({
-  selector: 'caelumpic-listagem',
-  templateUrl: './listagem.component.html',
-  styles: []
+	selector: 'caelumpic-listagem',
+	templateUrl: './listagem.component.html',
+	styles: []
 })
 export class ListagemComponent implements OnInit {
 
-  title = 'Caelum Pic Listagem';
-  listaFotos: Foto [] = [];
-  constructor( private fotoService: FotoService ){
-    this.fotoService.listar()
-    .subscribe(
-        // Arrow Function
-        fotosApi => {
-            this.listaFotos = fotosApi
-        }    
-    )
-  }
+	title = 'Caelum Pic Listagem';
+	listaFotos: Foto [] = [];
+	constructor( private fotoService: FotoService ){
+		this.fotoService.listar()
+		.subscribe(
+				// Arrow Function
+				fotosApi => {
+						this.listaFotos = fotosApi
+				}    
+		)
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  excluir(fotoApagada:Foto){
-    console.log(`Apagar ${fotoApagada.titulo}`);
-    this.fotoService.deletar(fotoApagada).subscribe(
-      ()=>{
-         console.log('apagou');
-         this.listaFotos.remove(fotoApagada._id);
-      }
-    );
-  }
+	excluir(fotoApagada:Foto){
+		console.log(`Apagar ${fotoApagada.titulo}`);
+		this.fotoService.deletar(fotoApagada).subscribe(
+				()=>{
+					console.log('apagou');
+					this.listaFotos = this.listaFotos.filter(
+						(foto)=>{
+							if(foto != fotoApagada){
+								return foto;
+							}
+						}
+					);
+					// this.listaFotos = this.listaFotos.filter(foto => foto != fotoApagada);
+				}
+		);
+	}
 
 }
